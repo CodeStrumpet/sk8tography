8a4ddfd560f554dc75edf891a5336f86e6eb8439
Paul Mans <gidentity@gmail.com>
2013-07-08 11:43:31 -0700

    adding multiple tricks, rough

--- public/js/controllers/TagClipsCtrl.js
+++ public/js/controllers/TagClipsCtrl.js
@@ -53,6 +53,83 @@ function TagClipsCtrl($scope, $http, $injector, $dialog, YoutubeService) {
   $scope.inputs.push($scope.skaterInput);
 
 
+  var newTrickInput = function() {
+    return {
+      name : "Trick(s)",
+      value : "",
+      type : "text",
+      helpText : "",
+      typeahead : "value.name for value in getMatches($viewValue, $index)",
+      typeaheadResults : [],
+      selectedObj : null,
+      entityName : "TrickType",
+      templateUrl: 'partials/addNewSkater',
+      controller: 'AddNewTrickTypeCtrl',
+      multipleField : true,
+      dropdownOptions : ["Ledge", "Flatground", "Rail", "Gap", "Transition"],
+      isLastTrick : true,
+      typeaheadFetch : function(searchText, successFunction) {
+
+        var url = '/api/trickTypes';
+        if (searchText) {
+          url = url + "?q=" + searchText;
+        }
+
+        return $http.get(url).then(function(response) {
+          var trickTypes = response.data.trickTypes;
+          return successFunction(trickTypes);
+        });
+      },
+      checkValidity : function() {
+        var valid = false;
+        for (var i = 0; i < this.typeaheadResults.length; i++) {
+          if (this.typeaheadResults[i].name.toLowerCase() === this.value.toLowerCase()) {
+            console.log("exact typeahead match!");
+            this.selectedObj = this.typeaheadResults[i];
+            valid = true;
+            break;
+          } 
+        }
+        // reset selectedObj to null if we don't have a match
+        if (!valid) {
+          this.selectedObj = null;
+        }
+      }
+    };
+  };
+
+  // initialize the tricks array with one trick
+  $scope.tricks = [newTrickInput()];
+
+  $scope.inputs.push($scope.tricks[0]);
+
+
+  $scope.addAnotherTrick = function(index) {
+
+    var newTrick = newTrickInput();
+
+    var lastTrick = $scope.tricks.slice(-1)[0]
+
+    // figure out what index the last trick is at
+    var i;
+    for (i = 0; i < $scope.inputs.length; i++) {
+      if ($scope.inputs[i] == lastTrick) {
+        break;
+      }
+    }
+
+    // insert new trick after last trick
+    $scope.inputs.splice(i+1, 0, newTrick);
+
+    // make sure all the other tricks know they are not the last trick
+    for (i = 0; i < $scope.tricks.length; i++) {
+      $scope.tricks[i].isLastTrick = false;
+    }
+
+    // add the new trick to the end of our tricks array
+    $scope.tricks.push(newTrick);
+  };
+
 
   $scope.clipsBecameAvailable = function() {
   	$scope.clipsAvailable = true;
@@ -88,7 +165,7 @@ function TagClipsCtrl($scope, $http, $injector, $dialog, YoutubeService) {
       });
     }
 
-
+    // cue the video if our console is ready to play videos
   	if (YoutubeService.playerIsReady) {
   		YoutubeService.cueClip(clip);
   	}
